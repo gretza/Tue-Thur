6b8aca64b4e93748b77ab2601ca7a7ccd27493ea
gret.za <greta.zaveckaite@gmail.com>
2022-11-14 20:53:28 +0200

    naujas

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let pirmasSkaicius = 123456789;
+let antrasSkaicius = 5;
+let rezultatas = pirmasSkaicius + antrasSkaicius;
+
+console.log(pirmasSkaicius + antrasSkaicius);
+console.log(rezultatas);
